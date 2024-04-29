9384a64c79df73ffe84fd88589be9c3ddeaa5aac
mitukou1109 <mitukou1109@gmail.com>
2024-04-29 22:10:18 +0900

    fix noise addition

--- src/imi_assignment_1/transforms.py
+++ src/imi_assignment_1/transforms.py
@@ -4,4 +4,4 @@ _rng = np.random.default_rng()
 
 
 def apply_noise(x: np.ndarray, prob: float) -> np.ndarray:
-    return np.where(_rng.random(x.shape) >= prob, x, _rng.choice([1, -1]))
+    return np.where(_rng.random(x.shape) >= prob, x, _rng.choice([1, -1], size=x.shape))
